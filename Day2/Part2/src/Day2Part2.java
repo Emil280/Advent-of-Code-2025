2cd31d64c23ce64b09a69cba02b12b92c1e661a3
Emil280 <79112726+Emil280@users.noreply.github.com>
2025-12-02 14:15:16 +0000

    Improved Part 2 solution
    After asking chatgpt about efficieny improvements it gave me this linear time method for checking for string repetitions which is a notable improvement in time complexity over my own solution. It also only adds the id if it contains periodically recurring numbers making it a valid solution.

--- Day2/Part2/src/Day2Part2.java
+++ Day2/Part2/src/Day2Part2.java
@@ -5,6 +5,12 @@ import java.nio.file.Path;
 import java.util.regex.Pattern;
 
 public class Day2Part2 {
+    public static boolean isRepeated(String s) {
+        String doubled = s + s;
+        // search for s starting from index 1
+        return doubled.indexOf(s, 1) != s.length();
+    }
+
     public static void main(String[] args) {
         try {
             BufferedReader myReader = new BufferedReader(new FileReader(Path.of("Day2\\Part2\\src\\Input.txt").toString()));
@@ -21,12 +27,8 @@ public class Day2Part2 {
                 sRange = ranges[i].split("-");
                 for (long n = Long.parseLong(sRange[0]); n <= Long.parseLong(sRange[1]); n++){
                     id = Long.toString(n);
-                    for (int s = 1; s<=(id.length()/2); s++){
-                        sub = id.substring(0, s);
-                        if (id.matches("(" + Pattern.quote(sub) + ")+")){
-                            ans += n;
-                            break;
-                        }
+                    if (isRepeated(id)) {
+                        ans+= n;
                     }
                 }
             }
